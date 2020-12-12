5df80053f4f0aaa82166d09a892f8fa5988f652d
SemihDurmus <semihdurmus@yahoo.com>
2020-12-12 17:12:27 +0100

    User already registration check

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,17 +1,26 @@
 const User = require("../models/UserModel");
 const bcrypt = require("bcryptjs");
+const validator = require("express-validator");
 
 exports.authRegister = async (req, res) => {
   const { firstName, lastName, email, password } = req.body;
   console.log("Fields", firstName, lastName, email, password);
-  //TODO-1: Validate fields
-  //TODO-2: check if already registered
-  //TODO-3: crypt password
-  //TODO-4:+ save the user to DB
 
-  const salt = await bcrypt.genSalt(10);
+  //TODO-1: Validate fields
+
+  //TODO-2:+ check if user is already registered
+  const userData = await User.findOne({ email }); //Bring this record with the email we send
+
+  if (userData) {
+    return res
+      .status(400)
+      .json({ errors: [{ message: "User already exists" }] });
+  }
+
+  //TODO-3:+ crypt password
+  const salt = await bcrypt.genSalt(10); // or w/o await bcrypt.genSaltSync(10)
   const newPassword = await bcrypt.hash(password, salt);
-  console.log(salt, newPassword);
+  //console.log("salt : ", salt, " Crypted password : ", newPassword);
   const user = new User({
     firstName,
     lastName,
@@ -19,6 +28,7 @@ exports.authRegister = async (req, res) => {
     password: newPassword, //crypted password
   });
 
+  //TODO-4:+ save the user to DB
   await user.save();
   //TODO: error handling for saving
   res.send("Register Completed.");
