26e0d043da0dbca1ba18fa4cbf675817fd4b1ff0
SemihDurmus <semihdurmus@yahoo.com>
2020-12-13 17:32:37 +0100

    Create react and package.json changes

--- routes/AuthRouter.js
+++ routes/AuthRouter.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const { check } = require("express-validator");
+const AuthController = require("../controllers/AuthController");
 
 const router = express.Router();
 
-const AuthController = require("../controllers/AuthController");
 // routes for /api/auth
 
 /**
@@ -14,11 +14,12 @@ const AuthController = require("../controllers/AuthController");
 router.post(
   "/register",
   [
-    check(
-      "password",
-      "Please enter a password with 6 or more characters"
-    ).isLength({ min: 6 }),
-    check("email", "Please enter a valid email").isEmail(),
+    check("password", "Please enter a password with 6 and more chars").isLength(
+      {
+        min: 6,
+      }
+    ),
+    check("email", "Please enter a valid email!").isEmail(),
   ],
   AuthController.authRegister
 );
@@ -26,19 +27,17 @@ router.post(
 /**
  * @route   POST /api/auth/login
  * @desc    Login endpoint
- * @access  Private
+ * @access  Public
  */
 router.post(
   "/login",
   [
-    check(
-      "email",
-      "Please enter a password with 6 or more characters"
-    ).isEmail(),
-    check(
-      "password",
-      "Please enter a password with 6 or more characters"
-    ).isLength({ min: 6 }),
+    check("email", "Please enter a valid email!").isEmail(),
+    check("password", "Please enter a password with 6 and more chars").isLength(
+      {
+        min: 6,
+      }
+    ),
   ],
   AuthController.authLogin
 );
